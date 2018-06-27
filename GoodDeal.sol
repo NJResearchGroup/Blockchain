c40d0e6b3e7729b1f9b2159a231133825038d904
NJResearchGroup <40623079+NJResearchGroup@users.noreply.github.com>
2018-06-27 15:05:33 +0800

    Add files via upload

---
+++ GoodDeal.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.23;
+
+
+contract Good_Deal_top {
+ 
+  //say Good deal Daryl.top
+  function say() public pure returns (string) {
+    return "Good deal Daryl.top";
+  }
+ 
+  //print name
+  function print(string name) public pure returns (string) {
+    return name;
+  }
+}
