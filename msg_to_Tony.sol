3a6c13a0cb6c78c5b48c306300192eb9a6548603
NJResearchGroup <40623079+NJResearchGroup@users.noreply.github.com>
2018-07-13 12:03:40 +0800

    Update msg_to_Tony.sol

--- msg_to_Tony.sol
+++ msg_to_Tony.sol
@@ -1,18 +1,16 @@
 pragma solidity ^0.4.23;
 
 
-contract msg_to_Tony {
+contract Msg_to_Tony {
  
   //say msg to Tony
-  function key() public pure returns (bytes) {
-    return '\x84\x94*\xf3\x8f\x8eS\x1c\xc6\x84\xcc,\x9e\xdf!bz\xaaZ\x97~\x12\xbcw\x9b\n\x8c\xce\xc9\xddF\xc8)\xf0>\xdb\x0b\x03\x1b\xa0\xb5*o\xf2Z\x11\xa8\xa3"\xd4\xfcZ.\x01L\ngr\xc0)\x9e\xd2\x95%';
+  function key() public pure returns (string) {
+      return "3ae07527e5564346cb3ed92547175d0779cebb2f4788726af070086c7e71bae5e32ddba3c759ed6006af0b84b01f1deaae311a7855bcf4e1bc33258d3c0fefa6";
   }
   function A() public pure returns (string) {
     return "164872f0a0f63e828a62c9457f232d6f2895e18dc71c4a80595f3321c65a45770d2bce9cd2d9c04912dfbd4171821b8b96c11eaf1141e6bce993625e838c48b7";
   }
   function D() public pure returns (string) {
-    // return "164872f0a0f63e828a62c9457f232d6f2895e18dc71c4a80595f3321c65a45770d2bce9cd2d9c04912dfbd4171821b8b96c11eaf1141e6bce993625e838c48b7";
     return "3df5b836aa19193ebc0c391a90f837ec00c173def06fea1ec662dc525dae4c15" ;
-    //b'_q\xd2\xcf\xb0\x9f}\t\xf0:\x17\xdb\xa9\xe4\x98V\xd75\x1c\x98\xeb\xdd>\xba\xde\x81\xb1\x03N\x99-'";
   }
 }
